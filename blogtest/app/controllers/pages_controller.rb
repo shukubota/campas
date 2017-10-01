39550e8f0bb8e294f05c58fa2810dcdd25234d65
Kubota <kubotashu0421@gmail.com>
2017-10-01 23:39:34 +0900

    commit

--- blogtest/app/controllers/pages_controller.rb
+++ blogtest/app/controllers/pages_controller.rb
@@ -1,11 +1,29 @@
 class PagesController < ApplicationController
   def index
   	@page=Page.all
+
+	ids=REDIS.zrevrange "pages",0,19, withscores: true
+	
+	@ids=ids
+	@ids_inv=ids.transpose
+
+	@pages=@ids_inv[0].map{|id|Page.find(id)}
   end
 
   def show
 	@page=Page.find(params[:id])
 	@user=current_user.email
+	#REDIS.zincrby "pages/daily/#{Date.today.to_s}",1,@page.id
+	REDIS.zincrby "pages",1,@page.id
+	ids=REDIS.zrevrange "pages",0,19, withscores: true
+	#@pv=REDIS.get "pages/daily/#{Date.today.to_s}/#{@page.id}"
+	
+	#@scores=scores
+	@ids=ids
+	@ids_inv=ids.transpose
+
+	#@pages=Page.where(id: @ids_inv[0])
+	@pages=@ids_inv[0].map{|id|Page.find(id)}
   end
 
 
@@ -25,6 +43,7 @@ class PagesController < ApplicationController
 	def destroy
 		@page=Page.find(params[:id])
 		@page.destroy
+		REDIS.zrem "pages" ,@page.id
 		redirect_to root_path
 	end
 
