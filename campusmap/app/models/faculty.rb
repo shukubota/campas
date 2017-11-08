206693a75819581787d2ac0cbc12ba65125a895b
Kubota <kubotashu0421@gmail.com>
2017-11-08 20:42:48 +0900

    kadai2修正

--- campusmap/app/models/faculty.rb
+++ campusmap/app/models/faculty.rb
@@ -1,6 +1,6 @@
 class Faculty 
   include Neo4j::ActiveNode
   property :name, type: String
-	has_many :in ,:universities,type: :has
+	has_many :in ,:universities,type: :has, model_class: :University
 	validates :name,uniqueness: true
 end
--- campusmap/app/models/university.rb
+++ campusmap/app/models/university.rb
@@ -1,6 +1,6 @@
 class University 
   include Neo4j::ActiveNode
   property :name, type: String
-	has_many :out,:Faculties,type: :has
+	has_many :out,:faculties,type: :has, model_class: :Faculty
 	validates :name,uniqueness: true	
 end
